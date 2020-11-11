4cba67591b3cfaa948cfff2f8743c3a9093cc2ab
Tomikimi Ogoli <tomikimiogoli@yahoo.com>
2020-11-11 14:26:07 +0100

    Added PUT and DELETE operations to Event Route

--- routes/event.js
+++ routes/event.js
@@ -3,6 +3,7 @@ const { fileUploadControl } = require("../middleware/uploadController");
 const _ = require("lodash");
 const fs = require("fs");
 const express = require("express");
+const { pick } = require("lodash");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -29,6 +30,7 @@ router.post("/", fileUploadControl.single("file"), async (req, res) => {
   event.title = req.body.title;
   event.eventDate = req.body.eventDate;
   event.eventTime = req.body.eventTime;
+  event.eventDetails = req.body.eventDetails;
   event.eventImage.data = fs.readFileSync(req.file.path);
   event.eventImage.contentType = "image/jpeg";
 
@@ -40,4 +42,35 @@ router.post("/", fileUploadControl.single("file"), async (req, res) => {
 router.put("/:id", fileUploadControl.single("file"), async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  const { error } = validateID(req.params);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let event = await Events.findById(req.params.id);
+  if (!event) return res.status(400).send("Event not Found");
+
+  event = await Events.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        title: req.body.title,
+        eventDate: req.body.eventDate,
+        eventTime: req.body.eventTime,
+        eventDetails: req.body.eventDetails,
+        eventImage: req.file.path,
+      },
+    },
+    { new: true }
+  );
+
+  res.send(_, pick(event, ["title", "eventDate", "eventTime"]));
+});
+
+router.delete("/:id", async (req, res) => {
+  const { error } = validateID(req.params);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const event = await Events.findByIdAndRemove(req.params.id);
+
+  res.send(_.pick(event, ["title", "eventDate", "eventTime"]));
 });
